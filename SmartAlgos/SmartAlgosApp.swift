be4441c56bd6d3ce59946b089308296b80443958
Paco Sainz <pacoswilliams@gmail.com>
2022-09-01 13:15:28 -0500

    Initial Commit

---
+++ SmartAlgos/SmartAlgosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartAlgosApp.swift
+//  SmartAlgos
+//
+//  Created by paco on 01/09/22.
+//
+
+import SwiftUI
+
+@main
+struct SmartAlgosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
